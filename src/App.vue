ca37b04dd80314c0902d65b9629c34f1dc7eb111
mincemaker <mincemaker@gmail.com>
2018-07-21 15:06:32 +0900

    fixes #2 vue2.5.16 (#3)
    * fixes #2 vue2.5.16
    
    * updated .gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,27 @@
 <template>
   <div id="app">
-    <button v-on:click="greet" class="pure-button pure-button-primary">draw</button>
-    <button v-on:click="reset" class="pure-button pure-button-primary">reset</button>
-
-    <div class="pure-g">
-      <div class="pure-u-md-3-5">
-        <div id="bingo">
-          <h2 id="aiko" v-if="name" class="animated">{{ name }}</h2>
-          <transition
-          name="custom-classes-transition"
-          enter-active-class="animated tada"
-          leave-active-class="animated bounceOutRight"
-          >
-
-          <img v-if="name" :src="image_url" :alt="name" />
-          </transition>
-        </div>
-      </div>
-
-      <div class="pure-u">
-        <div id="history" v-if="draw_lists.length > 0">
-          <h2>りれき</h2>
-          <ul>
-            <li v-for="name in draw_lists">
-              {{ name }}
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <Aiko />
   </div>
 </template>
 
 <script>
-import Shuffle from 'shuffle-array'
-import cards from './aiko.json'
-export default {
-  data () {
-    return {
-      name: '',
-      cards: cards,
-      image_url: '',
-      shuffled_cards: [],
-      draw_lists: [],
-      msg: 'test'
-    }
-  },
-  mounted: function() {
-    this.$nextTick(function() {
-      this.shuffled_cards = Shuffle(this.cards, { 'copy': true })
-    })
-  },
-  methods: {
-    greet: function() {
-      console.log('click sareta')
-      if (this.shuffled_cards.length === 0) {
-        alert('end');
-        return
-      }
+import Aiko from './components/Aiko.vue'
 
-      const drawed_card = this.shuffled_cards.pop();
-      console.log(drawed_card.name)
-      this.name = '';
-      setTimeout(() => {
-        this.name = `${drawed_card.id}: ${drawed_card.name}`;
-        this.image_url = drawed_card.image_url;
-        this.draw_lists.push(this.name);
-      }, 1000);
-    },
-    reset: function() {
-      this.shuffled_cards = Shuffle(this.cards, { 'copy': true });
-      this.name = '';
-      this.image_url = '';
-      this.draw_lists = [];
-    }
+export default {
+  name: 'app',
+  components: {
+    Aiko
   }
 }
 </script>
 
-<style lang="scss">
-h2 { letter-spacing: 0em; }
-img { max-width: 400px }
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
